e82846497870ea3d668a89528519ae2a6193d120
Chaitali D <cudandale@gmail.com>
2022-06-09 23:56:30 -0400

    Problem 28 is submitted

---
+++ Strings/Problem0028.js
@@ -0,0 +1,34 @@
+/**
+Implement strStr().
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+Clarification:
+What should we return when needle is an empty string? This is a great question to ask during an interview.
+
+For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+Example 1:
+
+Input: haystack = "hello", needle = "ll"
+Output: 2
+Example 2:
+
+Input: haystack = "aaaaa", needle = "bba"
+Output: -1
+
+Constraints:
+1 <= haystack.length, needle.length <= 104
+haystack and needle consist of only lowercase English characters.
+ */
+
+let strStr = function (haystack, needle) {
+  if (needle === "") {
+    return 0;
+  }
+
+  for (let i = 0; i < haystack.length; i++) {
+    let haySlice = haystack.slice(i, i + needle.length);
+    if (haySlice === needle) {
+      return i;
+    }
+  }
+  return -1;
+};
