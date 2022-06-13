e43ce6c10f5751c7f24eef9f1107ee52abd4e12c
Chaitali D <cudandale@gmail.com>
2022-06-12 23:54:56 -0400

    Problem 83 is completed

---
+++ List/Problem0083.js
@@ -0,0 +1,25 @@
+/**
+* Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+Example 1:
+
+
+Input: head = [1,1,2]
+Output: [1,2]
+Example 2:
+
+
+Input: head = [1,1,2,3,3]
+Output: [1,2,3]
+ */
+
+var deleteDuplicates = function (head) {
+  let c = head;
+  while (c && c.next) {
+    if (c.val == c.next.val) {
+      c.next = c.next.next;
+    } else {
+      c = c.next;
+    }
+  }
+  return head;
+};
